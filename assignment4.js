a3cc51e106d79aa1ce5aef156350f7a28eb29ebc
machadoh <hhm4@pitt.edu>
2017-04-05 22:35:37 -0400

    Last assignment

--- assignment4.js
+++ assignment4.js
@@ -11,7 +11,37 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-})();
+$(document).ready(function() {
+        $('.flexsearch-wrapper').on("input", function(){
+                $('#search_div').html("");
+		var input = $('.flexsearch-input').val();
+		if(input.length > 0){
+		    AutoComplete(input);
+		}
+		else if (input.length == 0){
+		    $('#search_div').html("");
+		}
+	    });
+    });
+
+function AutoComplete(input){
+    $.ajax({
+	    url: 'http://www.mattbowytz.com/simple_api.json?data=all',
+		dataType: 'json',
+		success: function(data){
+		$.each(data.data, function(key, val){
+			$.each(val, function(k, v){
+
+				if(v.toUpperCase().search(input.toUpperCase()) > -1){
+				    AddListItem(v);
+				}
+			    });
+		    });
+	    }
+        });
+}
+
+function AddListItem(ListItem){
+    $('#search_div').append('<li>' + ListItem + '</li>');
+}
+
